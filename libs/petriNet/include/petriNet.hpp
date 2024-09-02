7c2dfc7673e21dfc78429a3d6df3f728d5d4ff3b
Thomas Dost <stu203329@mail.uni-kiel.de>
2024-09-02 19:03:51 +0200

    SIMU: Models get now fully parsed into their respective classes.

--- libs/petriNet/include/petriNet.hpp
+++ libs/petriNet/include/petriNet.hpp
@@ -12,18 +12,39 @@ using ID = std::size_t;
 // TODO Those could be made into the same type
 // then we could just look up the ids in the arc in one map
 class Place {
+public:
+  Place(ID id, std::string&& label, std::vector<ID>&& arcs, std::size_t tokens) : 
+    id(id), label(label), arcs(arcs), tokens(tokens) {
+  }
+  Place(const Place&) = delete;
+//  TODO Check what the default move constructor does
+  Place(Place&&) = default;
+  Place& operator=(Place&&) = default;
+
+private:
   ID id;
   std::string label;
   std::vector<ID> arcs;
+  std::size_t tokens;
+
 };
 
 class Transition {
+public:
+  Transition(ID id, std::string&& label, std::vector<ID>&& arcs) : id(id), label(label), arcs(arcs) {
+  }
+  Transition(const Transition&) = delete;
+//  TODO Check what the default move constructor does
+  Transition(Transition&&) = default;
+  Transition& operator=(Transition&&) = default;
+
+private:
   ID id;
   std::string label;
   std::vector<ID> arcs;
 };
 
-class Arc {
+struct Arc {
   ID id;
   ID startID;
   ID endID;
@@ -35,12 +56,29 @@ class PetriNet
 
 public:
   void loadFromJSON(const std::filesystem::path& path);
+  //void addTransition(ID id, const Transition& transition) {
+  //  transitions.insert_or_assign(id, transition);
+  //}
 
+
+  void addTransition(ID id, Transition&& transition) {
+    transitions.insert_or_assign(id, std::move(transition));
+  }
+
+
+  void addPlace(ID id, Place&& place) {
+    places.insert_or_assign(id, std::move(place));
+  }
+
+
+  void addArc(ID id, const Arc& arc) {
+    arcs.insert_or_assign(id, arc);
+  }
 
 private: 
-  std::map<std::size_t, Place> places;
-  std::map<std::size_t, Transition> transitions;
-  std::map<std::size_t, Arc> arcs;
+  std::map<ID, Place> places;
+  std::map<ID, Transition> transitions;
+  std::map<ID, Arc> arcs;
  
 };
 }
--- libs/petriNet/src/petriNet.cpp
+++ libs/petriNet/src/petriNet.cpp
@@ -3,6 +3,7 @@
 #include "../include/petriNet.hpp"
 #include <iostream>
 #include <fstream>
+#include <print>
 
 void helloFromLib() {
   std::cout << "Hello from lib!\n";
@@ -21,8 +22,39 @@ namespace petrinet {
     json parsed_json;
     input_file >> parsed_json;
 
-    input_file.close();
+    auto shapes = parsed_json["shapes"];
+    for (const auto& s: shapes) {
+      if(s["type"] == "Rectangle") {
+        ID id = s["id"];
+        std::string label = s["label"];
+        std::vector<ID> arcIDs = s["arcIDS"];
+        std::println("Transition {} initialized with id {}", label, id);
+        for (ID i : arcIDs) {
+          std::println("Arc {}", i);
+        }
+        addTransition(id, Transition{id, std::move(label), std::move(arcIDs)});
+      } else if(s["type"] == "Circle") {
+        ID id = s["id"];
+        std::string label = s["label"];
+        std::vector<ID> arcIDs = s["arcIDS"];
+        std::size_t tokens = s["tokens"];
+        std::println("Place {} initialized with id {} and {} tokens", label, id, tokens);
+        for (ID i : arcIDs) {
+          std::println("Arc {}", i);
+        }
+        addPlace(id, Place{id, std::move(label), std::move(arcIDs), tokens});
+      }  else if (s["type"] == "Arc") {
+        ID id = s["id"];
+        ID startID = s["startID"];
+        ID endID = s["endID"];
+        ID edgeWeight = s["edgeWeight"];
+        std::println("Arc initialized with id {} from {} to {} width weight {}", id, startID, endID, edgeWeight);
 
-    std::cout << parsed_json.dump(4) << std::endl; 
+        addArc(id, {id, startID, endID, edgeWeight});
+      } else {
+        std::cerr << "UNK" << std::endl;
+      }
+    }
+    input_file.close();
   }
 }
