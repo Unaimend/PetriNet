4b90cfc93786f8e3734674b20cad79ee87377ff7
Thomas Dost <stu203329@mail.uni-kiel.de>
2024-09-09 11:26:45 +0200

    Moved real init code into own function
    This makes parameter passing and cli testable

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,5 @@
-#include <iostream>
-#include <filesystem>
-#include "../libs/petriNet/include/petriNet.hpp"
-#include <thread>
+int run(int, char**);
 
 int main(int argc, char **argv) {
-  auto filepath = std::filesystem::path{argv[1]};
-
-  petrinet::PetriNet p;
-  p.loadFromJSON(filepath);
-
-  p.simulateNShuffe(1'000'000);
-  //p.toDot("test.dot");
-  p.saveFinalTokenCount("FTC.json");
-  p.saveTokenHistory("TH.json");
-  p.saveReactionActivityCount("RAC.json");
-  p.saveReactionActivityHistory("RAH.json");
-  return 0;
+  return run(argc, argv);
 }
---
+++ src/run.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <filesystem>
+#include "../libs/petriNet/include/petriNet.hpp"
+#include <thread>
+
+int run(int argc, char** argv) {
+  auto filepath = std::filesystem::path{argv[1]};
+
+  petrinet::PetriNet p;
+  p.loadFromJSON(filepath);
+
+  p.simulateNShuffe(1'000'000);
+  //p.toDot("test.dot");
+  p.saveFinalTokenCount("FTC.json");
+  p.saveTokenHistory("TH.json");
+  p.saveReactionActivityCount("RAC.json");
+  p.saveReactionActivityHistory("RAH.json");
+  return 0;
+}
