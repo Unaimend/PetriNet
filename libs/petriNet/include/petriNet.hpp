cf0ac448083eb1197775f0d5db0b0105c02bcb6e
Thomas Dost <stu203329@mail.uni-kiel.de>
2024-09-10 15:27:07 +0200

    Fixes #1

--- libs/petriNet/include/petriNet.hpp
+++ libs/petriNet/include/petriNet.hpp
@@ -275,9 +275,8 @@ public:
       auto outSum = std::accumulate(outgoingTokens.begin(), outgoingTokens.end(), static_cast<std::size_t>(0));
       auto incSum = std::accumulate(incomingTokens.begin(), incomingTokens.end(), static_cast<std::size_t>(0));
 
-
       D(std::cout << *minIncToken << " " << incSum << " " << outSum << "\n";)
-      if(*minIncToken > 0 && incSum > outSum) {
+      if(!incomingTokens.empty() && *minIncToken > 0 && incSum > outSum) {
         M(const auto label = transitions.at(id).getLabel();)
         RAC(reactionActivity[label]++;)
         RAH(reactionActivityHistory[label].push_back(true);)
