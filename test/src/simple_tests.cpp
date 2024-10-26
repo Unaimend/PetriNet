a367126a644309883a21120d12efe40d677073a1
Thomas Dost <stu203329@mail.uni-kiel.de>
2024-10-26 16:09:10 +0200

    Added more tests

--- test/src/simple_tests.cpp
+++ test/src/simple_tests.cpp
@@ -8,18 +8,18 @@ int main() {
   using namespace boost::ut;
 
 
-  "path does not exist"_test = [] {
+   "path does not exist"_test = [] {
     petrinet::PetriNet p;
     expect(throws([&] { p.loadFromJSON("non_existing.json"); })) << "throws any exception";
   };
 
 
-  "malformed json"_test = [] {
+   "malformed json"_test = [] {
     petrinet::PetriNet p;
     expect(throws([&] { p.loadFromJSON("../../test/examples/loading/malformed_json.json"); })) << "throws nlohman::json exception";
   };
 
-  "circle loading sucess"_test = [] {
+   "circle loading sucess"_test = [] {
     petrinet::PetriNet p;
     p.loadFromJSON("../../test/examples/loading/circle.json");
     expect(p.getPlaces().size() == 1);
@@ -31,7 +31,7 @@ int main() {
   };
 
 
-  "rectangle loading sucess"_test = [] {
+   "rectangle loading sucess"_test = [] {
     petrinet::PetriNet p;
     p.loadFromJSON("../../test/examples/loading/rectangle.json");
     expect(p.getPlaces().size() == 0);
@@ -40,5 +40,36 @@ int main() {
 
     expect(p.getTransitions().at(1).getLabel() == "");
     expect(p.getTransitions().at(1).getID() == 1);
+  };
+  
+  "executing single reaction"_test = [] {
+    petrinet::PetriNet p;
+    p.loadFromJSON("../../test/examples/loading/smallest_example.json");
+    expect(p.getPlaces().size() == 2);
+    expect(p.getTransitions().size() == 1);
+    expect(p.getArcs().size() == 2);
+
+    p.setTokens(0, 10);
+    p.simulateGivenRxns({1, 1, 1, 1, 1});
+     
+    expect(p.getPlaces().at(0).getTokens() == 5);
+    expect(p.getPlaces().at(5).getTokens() == 5);
+  };
+  
+  "executing single reaction (2)"_test = [] {
+    petrinet::PetriNet p;
+    p.loadFromJSON("../../test/examples/loading/smallest_example.json");
+    expect(p.getPlaces().size() == 2);
+    expect(p.getTransitions().size() == 1);
+    expect(p.getArcs().size() == 2);
+
+    p.setTokens(0, 10);
+    // Test if the gradient works.
+    // After 5 executions the tokens for all places are 5.
+    // And thus the gradient is 0 so firing a 6th time should not do anything
+    p.simulateGivenRxns({1, 1, 1, 1, 1, 1});
+     
+    expect(p.getPlaces().at(0).getTokens() == 5);
+    expect(p.getPlaces().at(5).getTokens() == 5);
   };
 }
