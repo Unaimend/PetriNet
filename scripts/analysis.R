699b95db29e1aa264e38e054dc55aa42ee3d9f6b
Thomas Dost <stu203329@mail.uni-kiel.de>
2024-11-15 07:45:01 +0100

    Started refactoring r script

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -3,37 +3,44 @@ library(shinyjs)
 library(jsonlite)
 library(ggplot2)
 library(tidyverse)
-library(dplyr)
-THREADS <- 10
+THREADS <- 2
 # Func tion to read the JSON files and combine them into a data frame
-load_data <- function() {
-  data_list <- list()
 
+load_data_int <- function(prefix) {
+  data_list <- list()
   # Loop through your 10 JSON files and read them into a list
   for (i in 1:THREADS) {
-    file_name <- paste0("FTC_", i, ".json")
+    file_name <- paste0(prefix, i, ".json")
     data <- fromJSON(file_name)
     data_list[[i]] <- data
   }
+  return(data_list)
+}
 
-  # Combine the data into a data frame
+load_data <- function() {
+  data_list <- load_data_int("FTC_")
   df <- do.call(rbind, data_list)
-  return(df)
+  return(df %>% as_tibble())
 }
 
 
-load_data2 <- function() {
-  data_list <- list()
+load_token_history <- function() {
+  data_list <- load_data_int("TH_")
 
-  # Loop through your 10 JSON files and read them into a list
-  for (i in 1:THREADS) {
-    file_name <- paste0("TH_", i, ".json")
-    data <- fromJSON(file_name)
-    data_list[[i]] <- data
+  # Token history is a json file that contains a list for each metabolite
+  data2 <- data.frame(metabolite = character(), concentration = numeric(), run = factor())
+
+  for(i in 1:THREADS) { 
+    # We first convert each list to a df and then make it longer
+    data_temp <- as.data.frame(data_list[[i]], check.names = FALSE)
+    df_plot <- pivot_longer(data_temp, names_to = "metabolite", values_to = "concentration", cols = everything())
+    # We add the run it came from
+    df_plot$run <- as.factor(i)
+    # and just append it two the full df
+    data2 <- rbind(data2, df_plot)
   }
 
-  # Combine the data into a data frame
-  return(data_list)
+  return(data2)
 }
 
 
@@ -48,7 +55,8 @@ ui <-
         sidebarLayout(
           sidebarPanel(
             fluidPage(
-              selectInput("metabolite", "Select a Metabolite:",  choices = colnames(load_data()), selected = "13dpg_c")),
+              selectInput("metabolite", "Select a Metabolite:",
+                          choices = colnames(load_data()), selected = "13dpg_c")),
           ),
           mainPanel(
             fluidPage(
@@ -65,11 +73,11 @@ ui <-
         sidebarPanel(
           fluidPage(
             selectInput("metabolite2", "Select a Metabolite:",
-                        choices = c(colnames(load_data()), "all"), selected = "13dpg_c", multiple = TRUE), # nolint: line_length_linter.
+                        choices = c(colnames(load_data()), "all"), selected = "13dpg_c", multiple = TRUE), 
             selectInput("item2", "Select a run:",
-                        choices = c(1:length(load_data2()), "all"), selected = "all"), # nolint: line_length_linter.
+                        choices = c(1:length(unique(load_token_history()$run)), "all"), selected = "all"), 
             sliderInput("slider2", "Max x value:",
-                        min = 100, max = 1000, value = 1000)
+                        min = 0, max = 1000, value = 1000)
           ),
         ),
         mainPanel(
@@ -84,20 +92,12 @@ ui <-
 server <- function(input, output) {
   # Load data once when the app starts
   data <- load_data()
-  d <- load_data2()
-  data2 <- data.frame(metabolite = character(), concentration = numeric(), item = factor())
-  for(i in 1:THREADS){ 
-    data_temp <- as.data.frame(d[[i]], check.names = FALSE)
-    df_plot <- pivot_longer(data_temp ,names_to = "metabolite", values_to = "concentration", cols = everything())
-    df_plot$item <- as.factor(i)
-    data2 <- rbind(data2, df_plot)
-  }
+  token_history_data <- load_token_history()
+  
+
   output$barPlot <- renderPlot({
     selected_metabolite <- input$metabolite
-    df_plot <- filter(data2, metabolite == selected_metabolite)
-    df_plot$concentration <- as.numeric(df_plot$concentration)
-    # Plot the selected metabolite across all 10 files
-    ggplot(df_plot, aes(x = metabolite, y = concentration)) +
+    ggplot(token_history_data %>% filter(metabolite == selected_metabolite), aes(x = metabolite, y = concentration)) +
       geom_boxplot() +
       labs(x = "File", y = "Concentration",
            title = paste("Concentration of", selected_metabolite)) +
@@ -106,41 +106,26 @@ server <- function(input, output) {
 
   output$scatterPlot <- renderPlot({
     selected_metabolite <- input$metabolite2
-    if (selected_metabolite != "all") {
-      df_plot <- filter(data2, metabolite %in% selected_metabolite)
-    } else {
-      df_plot <- data2
-    }
     selected_run <- input$item2
-    df_plot <- df_plot %>%
-      group_by(item) %>% # nolint: object_usage_linter.
+    
+    token_history_data %>%
+      filter(if(selected_metabolite != "all") metabolite %in% selected_metabolite else TRUE) %>%
+      group_by(run) %>%
       mutate(rank = row_number()) %>%
-      ungroup()
-    print(head(df_plot))
-    if (selected_run != "all") {
-      df_plot <- filter(df_plot, item == selected_run) # nolint
-    }
-
-    df_plot <- filter(df_plot, rank < input$slider2)
-    df_plot$concentration <- as.numeric(df_plot$concentration)
-    # Plot the selected metabolite across all 10 files
-    if (selected_run != "all") {
-      p <-  ggplot(df_plot, aes(y = concentration, x = rank, color = metabolite))
-    } else {
-      p <- ggplot(df_plot, aes(y = concentration, x = rank, color = item))
-    }
-    p +
+      ungroup() %>%
+      filter(if(selected_run != "all") run == selected_run else TRUE)  %>%
+      filter(rank < input$slider2) %>%
+      ggplot(aes(y = concentration, x = rank, color = if (selected_run != "all") metabolite else run)) +
       geom_point() +
       labs(x = "File", y = "Concentration",
            title = paste("Concentration of",
-                         selected_metabolite, "of run", selected_run)) +
+                         selected_metabolite, "of run", selected_run),
+           color = "Run"
+           ) +
       theme_minimal()
   })
-
-
 }
 
 # Run the app
 shinyApp(ui = ui, server = server)
-#server()
 Sys.sleep(200)
